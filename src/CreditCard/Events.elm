10f6aace1efa8531ba9c193cf48e5df99ef119a2
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2017-01-04 22:49:30 -0600

    Expose CreditCard.Events

--- src/CreditCard/Events.elm
+++ src/CreditCard/Events.elm
@@ -1,11 +1,20 @@
 module CreditCard.Events exposing (onCCVFocus, onCCVBlur, updateCCVFocus)
 
+{-| Helpers function for all events related to focus/blur of CCV field for card flipping effects.
+See examples in the demo folder of the git repo for some example on how to use this module.
+
+# Events
+@docs updateCCVFocus, onCCVFocus, onCCVBlur
+-}
+
 import CreditCard.Internal exposing (CCVPosition(..), CardData, State(..), getStateValue)
 import Helpers.CardType
 import Html
 import Html.Events exposing (onBlur, onFocus)
 
 
+{-| Helper function to update the focus state of the ccv field in the internal state
+-}
 updateCCVFocus : Bool -> CardData model -> CardData model
 updateCCVFocus isFocused cardData =
     let
@@ -35,6 +44,9 @@ updateCCVFocus isFocused cardData =
         { cardData | state = updatedState }
 
 
+{-| `onFocus` event for the CCV field.
+This will update the focus state of the ccv field to `True` in the internal state
+-}
 onCCVFocus : (State -> msg) -> CardData model -> Html.Attribute msg
 onCCVFocus tagger cardData =
     let
@@ -44,6 +56,9 @@ onCCVFocus tagger cardData =
         onFocus (tagger updatedCardData.state)
 
 
+{-| `onBlur` event for the CCV field.
+This will update the focus state of the ccv field to `False` in the internal state
+-}
 onCCVBlur : (State -> msg) -> CardData model -> Html.Attribute msg
 onCCVBlur tagger cardData =
     let
